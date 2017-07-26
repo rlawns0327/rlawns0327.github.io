5cfcab301e620653371896c2e669b732ff999ea3
rlawns0327 <30451538+rlawns0327@users.noreply.github.com>
2017-07-26 11:06:46 +0900

    Create color.php

---
+++ color.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <form action="rlawns0327.github.io/color.php">
+    <h1>색상</h1>
+   <select>
+    <option>붉은색</option>
+    <option>검은색</option>
+    <option>파란색</option>
+    </select>
+    <input type="submit">
+    </from>
+  </body>  
+</html>
+    
